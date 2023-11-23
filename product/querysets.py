6107a6d14c6a71c2b3e2dcb2e76d1cb68e8d8aac
Mahsa R <mahsarm99@gmail.com>
2023-11-23 16:51:28 +0330

    complete in_stock queryset method

--- product/querysets.py
+++ product/querysets.py
@@ -7,4 +7,5 @@ class ProductQuerySet(QuerySet):
         return self.filter(stock__lt=10)
 
     def in_stock(self):
-        return self
+        """returns a queryset of products that their stock is at least 10"""      
+        return self.filter(stock__gte=10)
