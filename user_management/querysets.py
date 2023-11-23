c5d125b80adb3c4027749a4fecf8bbef2ffa5df7
Mahsa R <mahsarm99@gmail.com>
2023-11-23 16:59:09 +0330

    complete annotate_with_total_spending queryset method

--- user_management/querysets.py
+++ user_management/querysets.py
@@ -1,9 +1,11 @@
-from django.db.models import QuerySet
-
+from django.db.models import QuerySet, Sum, Count
+from django.db.models.functions import Coalesce
 
 class CustomerQuerySet(QuerySet):
     def annotate_with_total_spending(self):
-        return self
+        """returns a queryset of total spending of a customer"""
+        return self.annotate(
+            total_spending = Coalesce(Sum('order__total_price'), None))
 
     def annotate_with_order_count(self):
-        return self
+         return self
