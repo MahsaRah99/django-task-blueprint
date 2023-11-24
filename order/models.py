75fe4022fda461a26b79552989d0d899f2b3e55d
Mahsa R <mahsarm99@gmail.com>
2023-11-24 10:31:42 +0330

    complete calculate_total_price method in order model

--- order/models.py
+++ order/models.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 from django.db import models
 from order.enums import OrderStatus
 from order.querysets import OrderQuerySet
@@ -12,7 +13,10 @@ class Order(models.Model):
     objects = OrderQuerySet.as_manager()
 
     def calculate_total_price(self):
-        return 0
+        total_price = sum(item.product.price * item.quantity
+             for item in self.items.all()
+        )
+        return round(total_price,2)
 
     def pending(self):
         self.status = OrderStatus.PENDING
@@ -34,6 +38,7 @@ class Order(models.Model):
         self.status = OrderStatus.CANCELLED
         self.save()
 
+
 class OrderItem(models.Model):
     order = models.ForeignKey('order.Order', on_delete=models.CASCADE, related_name="items")
     product = models.ForeignKey('product.Product', on_delete=models.CASCADE)
@@ -42,6 +47,9 @@ class OrderItem(models.Model):
     def __str__(self):
         return f"{self.order.customer.user.username} - {self.product.name}"
 
+    # def create(self, *args, **kwargs):
+    #     pass
+    
     def save(self, *args, **kwargs):
         """You can not modify this method"""
         self.order.total_price = self.order.calculate_total_price()
